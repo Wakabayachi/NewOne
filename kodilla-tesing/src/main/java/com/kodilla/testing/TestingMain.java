6a9e2c2f6a0d6f64c29c4bbd3a220d78c0d6a9f2
Daniel Nastalski <maspa1987@gmail.com>
2024-02-18 17:46:26 +0100

    6.2 First singular test

--- kodilla-tesing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-tesing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,27 @@
+// com.kodilla.testing.TestingMain.java
+
 package com.kodilla.testing;
+
+import com.kodilla.testing.calculator.calculator;
 
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        calculator calculator = new calculator();
+
+        // Test for add method
+        int resultAdd = calculator.add(3, 7);
+        if (resultAdd == 10) {
+            System.out.println("Add method test: OK");
+        } else {
+            System.out.println("Add method test: Error!");
+        }
+
+        // Test for subtract method
+        int resultSubtract = calculator.subtract(10, 5);
+        if (resultSubtract == 5) {
+            System.out.println("Subtract method test: OK");
+        } else {
+            System.out.println("Subtract method test: Error!");
+        }
     }
 }
---
+++ kodilla-tesing/src/main/java/com/kodilla/testing/calculator/calculator.java
@@ -0,0 +1,13 @@
+// com.kodilla.testing.calculator.Calculator.java
+
+package com.kodilla.testing.calculator;
+
+public class calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+}
